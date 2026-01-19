81e95ec62f8038c1f47416877a066c36a2db1392
Troidem <onlinexmax@gmail.com>
2026-01-19 23:04:34 +0300

    refactor

--- tests/Csbf.Codegen.Tests/GoCodegenTests.cs
+++ tests/Csbf.Codegen.Tests/GoCodegenTests.cs
@@ -1,5 +1,4 @@
 ﻿using Csbf.Core;
-using Xunit.Abstractions;
 
 namespace Csbf.Codegen.Tests;
 
@@ -12,7 +11,7 @@ public class GoCodegenTests
         codegen.OnBegin();
         codegen.OnOp(new Op(OpKind.IncPtr, 2));
         codegen.OnEnd(new AnalysisResult(false, false));
-        
+
         var code = codegen.Emit();
 
         Assert.Equal("package main\n\n\nfunc main() {\n  mem := make([]byte, 30000)\n  dp := 0\n  dp += 2\n}\n", code);
