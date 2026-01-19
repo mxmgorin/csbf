73ca8fdfc1b44f14c235faba0fcdc29c6bf7241b
Troidem <onlinexmax@gmail.com>
2026-01-19 11:31:09 +0300

    fix: start message

--- src/Csbf.Cli/Program.cs
+++ src/Csbf.Cli/Program.cs
@@ -2,11 +2,12 @@
 
 using Csbf.Cli;
 
-Console.WriteLine("Welcome to csbf cli. Type 'help' for commands.");
+Console.WriteLine("Welcome to csbf cli.");
 var app = new App();
 if (args.Length == 0)
 {
     Console.WriteLine("Type 'help' for commands.");
+    Console.WriteLine();
     app.RunRepl();
 }
 else
